7f03a08d4b34048c1416d85ecde1dab539bcfac9
Ahmed Adel <ahmed45adel@yahoo.com>
2025-08-12 19:51:58 +0300

    unify token with frontend

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -5,7 +5,7 @@ const generateTokenAndSetCookie = (userId, res) => {
 		expiresIn: "15d",
 	});
 
-	res.cookie("jwt", token, {
+	res.cookie("chat-user", token, {
 		maxAge: 15 * 24 * 60 * 60 * 1000, // MS
 		httpOnly: true, // prevent XSS attacks cross-site scripting attacks
 		sameSite: "none", // CSRF attacks cross-site request forgery attacks
