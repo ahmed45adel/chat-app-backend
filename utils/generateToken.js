0d8ee40b4e6b53b35380e1cf2a4ada31231b6aa9
Ahmed Adel <ahmed45adel@yahoo.com>
2025-08-14 12:29:54 +0300

    add token for ably

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -1,17 +1,18 @@
 import jwt from "jsonwebtoken";
 
 const generateTokenAndSetCookie = (userId, res) => {
-	const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-		expiresIn: "15d",
-	});
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "15d",
+  });
 
-	res.cookie("chat-user", token, {
-		maxAge: 15 * 24 * 60 * 60 * 1000, // MS
-		httpOnly: true, 
-		sameSite: "none", // CSRF attacks cross-site request forgery attacks
-		secure: process.env.NODE_ENV === "production",
-		secure: true,
-	});
+  res.cookie("chat-user", token, {
+    maxAge: 15 * 24 * 60 * 60 * 1000, // 15 days
+    httpOnly: true,
+    sameSite: "none",
+    secure: process.env.NODE_ENV === "production",
+  });
+
+  return token; // Return the generated token
 };
 
 export default generateTokenAndSetCookie;
