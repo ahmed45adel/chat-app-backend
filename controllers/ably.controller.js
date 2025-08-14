302816aa68366b9fef28b97e795b985deccded1e
Ahmed Adel <ahmed45adel@yahoo.com>
2025-08-14 13:53:56 +0300

    test 3

--- controllers/ably.controller.js
+++ controllers/ably.controller.js
@@ -1,16 +1,13 @@
-import { ablyRealtime } from "../socket/socket.js";
-
 export const createTokenRequest = async (req, res) => {
-  // if (!req.user || !req.user._id) {
-  //   return res.status(401).json({ error: "Unauthorized - User not authenticated" });
-  // }
-
-  const clientId = req.user._id.toString();
+  const userId = req.query.userId;
+  if (!userId) {
+    return res.status(401).json({ error: "Unauthorized - User not authenticated" });
+  }
 
   try {
     // Use REST instance to create Ably token request
     const tokenRequest = await ablyRealtime.auth.createTokenRequest({
-      clientId,
+      clientId: userId.toString(),
       capability: { '*': ["publish", "subscribe"] },
       ttl: 3600000 // 1 hour expiration
     });
