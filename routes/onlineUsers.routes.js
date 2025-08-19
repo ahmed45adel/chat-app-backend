2acedccc41ce8b044422a9cbe3c882ba7a307d35
Ahmed Adel <ahmed45adel@yahoo.com>
2025-08-19 11:46:02 +0300

    fix router method

--- routes/onlineUsers.routes.js
+++ routes/onlineUsers.routes.js
@@ -5,6 +5,6 @@ const router = express.Router();
 
 router.post("/userConnected", userConnected);
 router.post("/userDisconnected", userDisconnected);
-router.get("/onlineUsers", getOnlineUsers);
+router.post("/onlineUsers", getOnlineUsers);
 
 export default router;
