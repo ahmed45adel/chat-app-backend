c0f7b6e10cad2e8b9a03a4f22556fbe4b559e94e
Ahmed Adel <ahmed45adel@yahoo.com>
2025-08-20 10:56:52 +0300

    switch to ably presence api

--- controllers/ably.controller.js
+++ controllers/ably.controller.js
@@ -3,16 +3,18 @@ import { ablyRealtime } from "../socket/socket.js";
 export const createTokenRequest = async (req, res) => {
   const userId = req.query.userId;
   if (!userId) {
-    return res.status(401).json({ error: "Unauthorized - User not authenticated" });
+    return res.status(401).json({ error: "Unauthorized" });
   }
 
   try {
-    // Use REST instance to create Ably token request
     const tokenRequest = await ablyRealtime.auth.createTokenRequest({
       clientId: userId.toString(),
-      capability: { '*': ["publish", "subscribe"] },
-      ttl: 3600000 // 1 hour expiration
+      capability: JSON.stringify({
+        "chat:*": ["publish", "subscribe", "presence"],
+      }),
+      ttl: 3600000, // 1 hour
     });
+
     res.status(200).json(tokenRequest);
   } catch (error) {
     console.error("Error creating Ably token request:", error);
