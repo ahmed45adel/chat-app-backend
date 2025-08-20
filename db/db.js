b288bb24e49e215a713960e9aa5a477607b7f43d
Ahmed Adel <ahmed45adel@yahoo.com>
2025-08-20 11:17:52 +0300

    handle buffering in db for vercel

--- db/db.js
+++ db/db.js
@@ -7,15 +7,14 @@ if (!cached) {
 }
 
 async function connectToMongoDB() {
-  if (cached.conn) {
-    return cached.conn;
-  }
+  if (cached.conn) return cached.conn;
+
   if (!cached.promise) {
     cached.promise = mongoose.connect(process.env.MONGO_DB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+      bufferCommands: false, 
     }).then((mongoose) => mongoose);
   }
+
   cached.conn = await cached.promise;
   return cached.conn;
 }
